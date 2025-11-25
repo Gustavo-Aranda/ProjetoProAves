46a5c493951aeb593032d1bef275a43768ce6854
Gustavo <gustavo.aranda01@fatec.sp.gov.br>
2025-11-25 11:11:40 -0300

    edição do plantel, nova aba ao invés de mostrar escrito.

---
+++ src/view/GerenciadorPlantel.java
@@ -0,0 +1,74 @@
+package view;
+
+import controller.EspecieController;
+import controller.GaiolaController;
+import controller.PlantelController;
+import model.Ave;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+public class GerenciadorPlantel extends JDialog {
+    private final PlantelController plantelController;
+    private final EspecieController especieController;
+    private final GaiolaController gaiolaController;
+
+    private JList<Ave> listaAvesVisual;
+    private DefaultListModel<Ave> listModel;
+
+    public GerenciadorPlantel(Frame parent, PlantelController plantelCtrl, EspecieController especieCtrl,
+                              GaiolaController gaiolaCtrl){
+        super(parent, "Gerenciador Plantel", true);
+        this.plantelController = plantelCtrl;
+        this.especieController = especieCtrl;
+        this.gaiolaController = gaiolaCtrl;
+
+        setSize(500,400);
+        setLocationRelativeTo(parent);
+        setLayout(new BorderLayout(10, 10));
+
+        add(criarPainelLista(), BorderLayout.CENTER);
+        add(new JLabel("Clique duas vezes para editar status ou detalhes", JLabel.CENTER), BorderLayout.SOUTH);
+
+        atualizarListaVisual();
+    }
+
+    private JScrollPane criarPainelLista(){
+        listModel = new DefaultListModel<>();
+        listaAvesVisual = new JList<>(listModel);
+        listaAvesVisual.setFont(new Font("Arial", Font.BOLD, 12));
+
+        listaAvesVisual.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    Ave aveSelecionada = listaAvesVisual.getSelectedValue();
+                    if (aveSelecionada != null) {
+                        abrirEdicao(aveSelecionada);
+                    }
+                }
+            }
+        });
+
+        return new JScrollPane(listaAvesVisual);
+    }
+
+    private void atualizarListaVisual(){
+        listModel.clear();
+        ArrayList<Ave> plantel = plantelController.getPlantel();
+        for (Ave ave : plantel){
+            listModel.addElement(ave);
+        }
+    }
+
+    private void abrirEdicao(Ave ave){
+        EditarAve dialogo = new EditarAve((Frame) getOwner(), plantelController, especieController, gaiolaController, ave);
+        dialogo.setVisible(true);
+
+        atualizarListaVisual();
+    }
+}
